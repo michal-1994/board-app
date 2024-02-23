45d607cb7a2e8fe4c9c1a68ab06d34f1d6924fad
Michal <michalgrzegorczyk04@gmail.com>
2024-02-23 09:59:38 +0100

    create tests to add, change and remove list

---
+++ src/__tests__/reducer.test.ts
@@ -0,0 +1,71 @@
+import { configureStore } from '@reduxjs/toolkit';
+import listsReducer, {
+    ListsState,
+    addList,
+    changeList,
+    removeList
+} from '../state/lists/reducer';
+
+const customInitialState: ListsState = {
+    lists: [
+        {
+            title: 'Custom List 1',
+            items: [{ text: 'Item 1' }, { text: 'Item 2' }]
+        },
+        {
+            title: 'Custom List 2',
+            items: [{ text: 'Item 1' }, { text: 'Item 2' }, { text: 'Item 3' }]
+        },
+        {
+            title: 'Custom List 3',
+            items: []
+        }
+    ]
+};
+
+let store = configureStore({
+    reducer: listsReducer
+});
+
+describe('lists reducer', () => {
+    beforeEach(() => {
+        store = configureStore({
+            reducer: listsReducer,
+            preloadedState: customInitialState
+        });
+    });
+
+    it('should handle addList', () => {
+        const newList = { title: 'New List', items: [] };
+        const action = addList(newList);
+        store.dispatch(action);
+
+        const state = store.getState() as ListsState;
+        expect(state.lists).toHaveLength(4);
+        expect(state.lists[3]).toEqual(newList);
+    });
+
+    it('should handle changeList', () => {
+        const action = changeList({
+            listId: 0,
+            title: 'Custom List 1 Changed'
+        });
+        store.dispatch(action);
+
+        const state = store.getState() as ListsState;
+        expect(state.lists).toHaveLength(3);
+        expect(state.lists[0].title).toEqual('Custom List 1 Changed');
+    });
+
+    it('should handle removeList', () => {
+        const action = removeList({ listId: 1 });
+        store.dispatch(action);
+
+        const state = store.getState() as ListsState;
+        expect(state.lists).toHaveLength(2);
+        expect(state.lists[1]).toEqual({
+            title: 'Custom List 3',
+            items: []
+        });
+    });
+});
